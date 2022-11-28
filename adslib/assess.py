beb1f41a21f538f4f350007edb39021468e9a6d2
Ivo Petrov <1414vo@gmail.com>
2022-11-28 17:49:14 +0000

    Fixed another issue

--- adslib/assess.py
+++ adslib/assess.py
@@ -117,6 +117,6 @@ def extract_feature_existence_in_box (building_data, tags, padding = 0.02, dista
     features = get_geometries_in_region(building_data, tags, padding = padding)
     if len(features) == 0:
         return False
-        centroids = features.to_crs(27700).geometry.centroid
+    centroids = features.to_crs(27700).geometry.centroid
     points = gpd.GeoSeries(building_data.apply(lambda x: Point(x.longitude, x.latitude), axis = 1)).set_crs(4326).to_crs(27700)
     return points.apply(lambda x: centroids.distance(x).min()) < distance_limit
