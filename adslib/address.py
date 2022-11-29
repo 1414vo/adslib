93774d6c1ce89ef78d097472f1ee76404b758dbc
Ivo Petrov <1414vo@gmail.com>
2022-11-29 14:38:23 +0000

    Fix attempt

--- adslib/address.py
+++ adslib/address.py
@@ -22,14 +22,14 @@ def cross_validation(features, target, split_fractions, loss = mse,  number_of_s
         model = sm.OLS(y_fit, X_fit)
         # Ridge regression due to multicollinearity
         fit = model.fit_regularized(alpha = alpha, L1_wt = 0)
-        scores.append(loss(fit.get_prediction(X_val), y_val))
+        scores.append(loss(fit.predict(X_val), y_val))
     
     test_model = sm.OLS(y_fit_val, X_fit_val)
     test_fit = test_model.fit_regularized(alpha = alpha, L1_wt = 0)
     
     results = {}
     results['validation_scores'] = np.array(scores)
-    results['test_score'] = loss(test_fit.get_prediction(X_test), y_test)
-    results['summary_frame'] = loss(test_fit.get_prediction(X_test).summary_grame(alpha = 0.05))
+    results['test_score'] = loss(test_fit.predict(X_test), y_test)
+    results['summary_frame'] = loss(test_fit.get_predict(X_test).summary_grame(alpha = 0.05))
     return results, test_model
 
